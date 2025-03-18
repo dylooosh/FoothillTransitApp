32bb250a8f0a07b9e9f50769e4588edaddc14c80
dyloosh <dyluong12@gmail.com>
2025-03-17 22:57:00 -0700

    Fix GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import '@mantine/core/styles.css';
 import './index.css';
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <Router basename="/FoothillTransitApp">
+        <App />
+      </Router>
+    </MantineProvider>
   </React.StrictMode>
 ); 
