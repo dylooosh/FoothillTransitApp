50f0246d335c21a894b2083e80bf093cd61fe823
dyloosh <dyluong12@gmail.com>
2025-03-17 22:59:35 -0700

    Switch to HashRouter for better GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import '@mantine/core/styles.css';
 import './index.css';
@@ -9,9 +9,9 @@ import { MantineProvider } from '@mantine/core';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
-      <Router basename="/FoothillTransitApp">
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </MantineProvider>
   </React.StrictMode>
 ); 
